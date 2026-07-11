86efc004f435a29f675e076a09c74286e2caaf12
Oleksandr Verezub <90050016+StepanChewbacca@users.noreply.github.com>
2026-07-11 16:30:23 +0300

    chore: remove obsolete Dynamo SQL queries

--- query_haze.sql
+++
@@ -1,5 +0,0 @@
-SELECT mp."heroId", mpsu."abilityId", mpsu."upgradeOrder", mp.id as player_id
-FROM match_player_skill_upgrades mpsu
-JOIN match_players mp ON mpsu."matchPlayerId" = mp.id
-WHERE mp."heroId" = 2
-ORDER BY mp.id, mpsu."upgradeOrder";
