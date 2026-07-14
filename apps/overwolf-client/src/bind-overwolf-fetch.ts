e2fcb05b94961998f25a194021554bd9523b46c9
Oleksandr Verezub <90050016+StepanChewbacca@users.noreply.github.com>
2026-07-14 18:16:47 +0300

    fix: remove detached fetch monkey patch

--- apps/overwolf-client/src/bind-overwolf-fetch.ts
+++
@@ -1,7 +0,0 @@
-const runtimeWindow = window;
-const nativeFetch = runtimeWindow.fetch;
-
-if (typeof nativeFetch === 'function') {
-  runtimeWindow.fetch = (input: RequestInfo | URL, init?: RequestInit): Promise<Response> =>
-    nativeFetch.call(runtimeWindow, input, init);
-}
